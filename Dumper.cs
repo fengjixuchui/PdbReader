369cf5256915f515aa9c8bc3dcd959b98477d46d
BlueSkeye <M2438FE89@gmail.com>
2022-07-17 08:49:54 +0200

    test bed initialized

--- Dumper.cs
+++ Dumper.cs
@@ -1,13 +1,41 @@
-﻿using PdbReader;
+﻿using System.Reflection;
+
+using PdbReader;
 
 namespace PdbDumper
 {
     public static class Dumper
     {
+        private static FileInfo _pdbFile;
+
         public static int Main(string[] args)
         {
-            Console.WriteLine("PdbDumper");
+            if (!ParseArgs(args)) {
+                Usage();
+                return 1;
+            }
+            Pdb pdv = new Pdb(_pdbFile);
             return 0;
+        }
+
+        private static bool ParseArgs(string[] args)
+        {
+            if (0 == args.Length) {
+                return false;
+            }
+            _pdbFile = new FileInfo(args[0]);
+            if (!_pdbFile.Exists) {
+                Console.WriteLine($"Input file '{_pdbFile.FullName}' doesn't exist.");
+                return false;
+            }
+            return true;
+        }
+
+        private static void Usage()
+        {
+            Assembly thisAssembly = Assembly.GetExecutingAssembly();
+
+            Console.WriteLine($"{thisAssembly.GetName().Name} <pdb file name>");
         }
     }
 }
--- Pdb.cs
+++ Pdb.cs
@@ -1,7 +1,39 @@
-﻿namespace PdbReader
+﻿
+namespace PdbReader
 {
     public class Pdb
     {
+        private const string SymbolCacheRelativePath = @"AppData\Local\Temp\SymbolCache";
+        private readonly FileInfo _pdbFile;
+
+        public Pdb(FileInfo target)
+        {
+            _pdbFile = target ?? throw new ArgumentNullException(nameof(target));
+            if (!_pdbFile.Exists) {
+                throw new ArgumentException($"Input file doesn't exist : '{_pdbFile.FullName}'");
+            }
+        }
+
+        /// <summary>Ensure the symbol cache directory exists otherwise create
+        /// it.</summary>
+        /// <returns>A descriptor for the cache directory.</returns>
+        /// <exception cref="BugException"></exception>
+        private static DirectoryInfo EnsureSymbolCacheDirectory()
+        {
+            string? userProfileDirectory = Environment.GetEnvironmentVariable("USERPROFILE");
+            if (null == userProfileDirectory)
+            {
+                throw new BugException();
+            }
+            DirectoryInfo result = new DirectoryInfo(
+                Path.Combine(userProfileDirectory, SymbolCacheRelativePath));
+            if (!result.Exists)
+            {
+                result.Create();
+                result.Refresh();
+            }
+            return result;
+        }
 
     }
 }
